aec19f853cec34e28eee3ed010bbe01828912176
Uiri <uiri00@gmail.com>
2013-11-14 00:33:53 -0500

    Add implementation of mkdir

---
+++ mkdir.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	goopt "github.com/droundy/goopt"
+	/*"io/ioutil"*/
+	"os"
+	/*"path/filepath"*/
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+var License = `License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law`
+
+func version() error {
+	fmt.Println(goopt.Suite + " " + goopt.Version)
+	fmt.Println()
+	fmt.Println("Copyright (C) 2013 " + goopt.Author)
+	fmt.Println(License)
+	os.Exit(0)
+	return nil
+}
+
+var mode uint32
+
+func setMode(m string) error {
+	smallend, err := strconv.ParseUint(m, 8, 32)
+	if err != nil {
+		fmt.Printf("Error occured while parsing mode: %v\n", err)
+		os.Exit(1)
+	}
+	mode = 1<<31 | uint32(smallend)
+	return nil
+}
+
+func createParents(dir string, verbose bool) bool {
+	error := false
+	dirs := strings.Split(dir, "/")
+	base := ""
+	for i := range dirs {
+		if verbose {
+			fmt.Printf("Creating directory %s\n", base+dirs[i])
+		}
+		err := os.Mkdir(base+dirs[i], os.FileMode(mode))
+		if err != nil && !os.IsExist(err) {
+			fmt.Printf("Error while creating directory '%s': %v\n", base+dirs[i], err)
+			error = true
+		}
+		base = base + dirs[i] + string(os.PathSeparator)
+	}
+	return error
+}
+
+func main() {
+	mode = 1<<31 | 0755
+	syscall.Umask(0)
+	goopt.Suite = "XQZ coreutils"
+	goopt.Author = "William Pearson"
+	goopt.Version = "Mkdir v0.1"
+	goopt.Summary = "Create each DIRECTORY, if it does not already exist."
+	goopt.Usage = func() string {
+		return fmt.Sprintf("Usage:\t%s [OPTION]... DIRECTORY...\n", os.Args[0]) + goopt.Summary + "\n\n" + goopt.Help()
+	}
+	goopt.Description = func() string {
+		return goopt.Summary + "\n\nUnless --help or --version is passed."
+	}
+	goopt.OptArg([]string{"-m", "--mode"}, "MODE", "Set file mode permissions", setMode)
+	parents := goopt.Flag([]string{"-p", "--parents"}, nil, "Make parent directories as needed, no error if existing", "")
+	verbose := goopt.Flag([]string{"-v", "--verbose"}, nil, "Output each directory as it is processed", "")
+	goopt.NoArg([]string{"--version"}, "outputs version information and exits", version)
+	goopt.Parse(nil)
+	error := false
+	for i := range os.Args[1:] {
+		if os.Args[i+1][0] == '-' {
+			continue
+		}
+		if os.Args[i][0] == '-' && os.Args[i][len(os.Args[i])-1] == 'm' {
+			continue
+		}
+		if *parents {
+			if createParents(os.Args[i+1], *verbose) {
+				error = true
+			}
+			continue
+		}
+		if *verbose {
+			fmt.Printf("Creating directory %s\n", os.Args[i+1])
+		}
+		err := os.Mkdir(os.Args[i+1], os.FileMode(mode))
+		if err != nil {
+			fmt.Println("Error creating direction %s: %v\n", os.Args[i+1], err)
+			error = true
+		}
+	}
+	if error {
+		os.Exit(1)
+	}
+	return
+}
