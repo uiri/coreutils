3880f6131a41e0fea3798aab24fe10ddcd724d59
Aaron Muir Hamilton <aaron@correspondwith.me>
2013-11-16 03:55:56 +0000

    Add explicit exit to main.

--- sleep/main.go
+++ sleep/main.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	goopt "github.com/droundy/goopt"
 	"os"
-	"time"
-	"strings"
 	"strconv"
+	"strings"
+	"time"
 )
 
 var License = `License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
@@ -34,15 +34,11 @@ func parseDuration(s string) time.Duration {
 		if strings.HasSuffix(s, "d") {
 			t := strings.Split(s, "d")[0]
 			n, nerr := strconv.ParseFloat(t, 64)
-			if nerr != nil {
-				frown("invalid time interval ‘" + s + "’")
-			}
+			if nerr != nil { frown("invalid time interval ‘" + s + "’") }
 			d = time.Duration(86400000000000 * n) // Ugly hack? seems that time.Duration is a nanosecond, and multiplying by anything just means that precision is your multiplicand.
 		} else {
 			n, nerr := strconv.ParseFloat(s, 64)
-			if nerr != nil {
-				frown("invalid time interval ‘" + s + "’")
-			}
+			if nerr != nil { frown("invalid time interval ‘" + s + "’") }
 			d = time.Duration(n) * time.Second
 		}
 
@@ -59,17 +55,12 @@ func main() {
 		return fmt.Sprintf("Usage of %s:\n\t   %s NUMBER[SUFFIX]\n\tor %s OPTION\n", os.Args[0], os.Args[0], os.Args[0]) +
 			goopt.Summary + "\n\n" + goopt.Help()
 	}
-	goopt.Description = func() string {
-		return goopt.Summary + "\n\nUnless an option is passed to it."
-	}
+	goopt.Description = func() string { return goopt.Summary + "\n\nUnless an option is passed to it." }
 	goopt.NoArg([]string{"-v", "--version"}, "outputs version information and exits", version)
 	goopt.Parse(nil)
-	if len(os.Args) == 1 {
-		frown("missing operand")
-	}
+	if len(os.Args) == 1 { frown("missing operand")	}
 	var d time.Duration
-	for i := range os.Args[1:] {
-		d += parseDuration(os.Args[i+1])
-	}
+	for i := range os.Args[1:] { d += parseDuration(os.Args[i+1]) }
 	time.Sleep(d)
+	os.Exit(0)
 }
